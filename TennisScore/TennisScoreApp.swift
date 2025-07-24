f5de4ff59ee72faa50e9002411394d4ffd1ec50e
casalecchi <felipevilela191@gmail.com>
2025-07-24 15:45:12 -0300

    Initial Commit

---
+++ TennisScore/TennisScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisScoreApp.swift
+//  TennisScore
+//
+//  Created by Felipe Casalecchi on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TennisScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
